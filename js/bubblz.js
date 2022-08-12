42081e655a52d5f4e6bccd493e0dc2b97bcecb41
MaximeMonnier <maximemonnier39@gmail.com>
2022-08-12 11:23:07 +0400

    first commit

---
+++ js/bubblz.js
@@ -0,0 +1,32 @@
+const cunterDisplay = document.querySelector("h3");
+
+let cunter = 0;
+
+console.log(cunterDisplay);
+
+const bubbleMaker = () => {
+  const bubble = document.createElement("span");
+  bubble.classList.add("bubble");
+  document.body.appendChild(bubble);
+
+  const size = Math.random() * 200 + 100 + "px";
+  bubble.style.height = size;
+  bubble.style.width = size;
+
+  bubble.style.top = Math.random() * 100 + 50 + "%";
+  bubble.style.left = Math.random() * 100 + "%";
+  const plusMinus = Math.random() > 0.5 ? 1 : -1;
+  bubble.style.setProperty("--left", Math.random() * 100 * plusMinus + "%");
+
+  bubble.addEventListener("click", () => {
+    cunter++;
+    cunterDisplay.textContent = cunter;
+    bubble.remove();
+  });
+
+  setTimeout(() => {
+    bubble.remove();
+  }, 8000);
+};
+
+setInterval(bubbleMaker, 300);
